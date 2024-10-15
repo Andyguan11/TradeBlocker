14204d17fb9d768c07e4ee37cbb7de61442a72f6
Andyguan11 <143280521+Andyguan11@users.noreply.github.com>
2024-10-14 22:31:47 -0500

    fdf

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  webpack: (config, { isServer }) => {
-    // Add any custom webpack configurations here if needed
-    return config
-  },
 }
 
 module.exports = nextConfig
