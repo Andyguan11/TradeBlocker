59c00b888c56df065d3c6890a9acb9020a0c76a2
Andyguan11 <143280521+Andyguan11@users.noreply.github.com>
2024-10-16 17:42:45 -0500

    ml

--- next.config.js
+++ next.config.js
@@ -1,14 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  async rewrites() {
-    return [
-      {
-        source: '/api/check-extension-connection',
-        destination: '/api/check-extension-connection',
-      },
-    ]
-  },
+  // If you're not using a 'src' directory, you might need to specify the directory where your pages are located
+  // For example:
+  // pageExtensions: ['js', 'jsx', 'ts', 'tsx'],
+  // dir: './',
 }
 
 module.exports = nextConfig
