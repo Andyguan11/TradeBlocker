9b3be8b29a6dcb52ddbad88ef7ae9a89b54ea9ac
Andyguan11 <143280521+Andyguan11@users.noreply.github.com>
2024-10-16 17:32:01 -0500

    Replace development branch with files from TradeBlocker before

--- Gemfile
+++
@@ -1,5 +0,0 @@
-source 'https://rubygems.org'
-
-ruby '3.1.2'
-
-# rest of your Gemfile contents
--- cypress/integration/app.spec.js
+++
@@ -1,7 +0,0 @@
-describe('App E2E', () => {
-  it('should load the app', () => {
-    cy.visit('http://localhost:3000');
-    cy.contains('Your App Title').should('be.visible');
-    // Add more assertions based on your app's content
-  });
-});
