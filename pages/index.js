f82f969eed5cf8e0ccbc1d1d3cd44876639fcb6d
Andyguan11 <143280521+Andyguan11@users.noreply.github.com>
2024-10-16 17:16:24 -0500

    testing

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import AuthPage from '../components/AuthPage'
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to TradeBlocker</h1>
+      <AuthPage />
+    </div>
+  )
+}
