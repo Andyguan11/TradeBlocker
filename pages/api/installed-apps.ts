eec54122da9f3f3d76372b25f10e3bbc82df158f
Andyguan11 <143280521+Andyguan11@users.noreply.github.com>
2024-10-16 22:01:17 -0500

    jj

--- pages/api/installed-apps.ts
+++ pages/api/installed-apps.ts
@@ -4,20 +4,30 @@ import { promisify } from 'util';
 
 const execAsync = promisify(exec);
 
+interface WindowsApp {
+  Name: string;
+  PackageFullName: string;
+}
+
+interface MacApp {
+  _name: string;
+  bundle_id: string;
+}
+
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   try {
     const platform = process.platform;
-    let apps = [];
+    let apps: { name: string; identifier: string }[] = [];
 
     switch (platform) {
       case 'win32':
         const { stdout: winApps } = await execAsync('powershell "Get-AppxPackage -AllUsers | Select-Object Name, PackageFullName | ConvertTo-Json"');
-        apps = JSON.parse(winApps).map((app: { Name: any; PackageFullName: any; }) => ({ name: app.Name, identifier: app.PackageFullName }));
+        apps = JSON.parse(winApps).map((app: WindowsApp) => ({ name: app.Name, identifier: app.PackageFullName }));
         break;
       case 'darwin':
         const { stdout: macApps } = await execAsync('system_profiler SPApplicationsDataType -json');
-        const macAppData = JSON.parse(macApps).SPApplicationsDataType;
-        apps = macAppData.map((app: { _name: any; bundle_id: any; }) => ({ name: app._name, identifier: app.bundle_id }));
+        const macAppData = JSON.parse(macApps).SPApplicationsDataType as MacApp[];
+        apps = macAppData.map((app: MacApp) => ({ name: app._name, identifier: app.bundle_id }));
         break;
       // Note: Android and iOS detection cannot be done server-side in this context
       default:
